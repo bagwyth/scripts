47f6d943dc236a0b0fbe71b5a2ef648bfb582a12
Ben Hummerstone <benhu@microsoft.com>
2017-04-06 14:24:50 +0100

    New Scripts

---
+++ GetServiceFromVM.ps1
@@ -0,0 +1,15 @@
+﻿$rg = "vs2017"
+$vm = "bhvs2017"
+$svc = "w32time"
+
+Set-AzureRmVMCustomScriptExtension -ResourceGroupName $rg `
+                                   -VMName $vm `
+                                   -FileUri "https://raw.githubusercontent.com/bhummerstone/Powershell/master/getservice.ps1" `
+                                   -Name "getservice" `
+                                   -Location "westeurope" `
+                                   -Run "getservice.ps1" `
+                                   -Argument $svc 
+
+
+$output = Get-AzureRmVMDiagnosticsExtension -ResourceGroupName $rg -VMname $vm -Name "getservice" -Status
+$output.SubStatuses[0]
